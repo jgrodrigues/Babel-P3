9378a6714e624f50fe3d77bdb705261117088126
Jonas Rodrigues <jg.rodrigues@campus.fct.unl.pt>
2018-06-01 02:09:15 +0100

    Fix bugs

--- Babel.js
+++ Babel.js
@@ -346,7 +346,7 @@ class Lesson {
 		this.nScreens = 0;
 		this.id = id;
 		this.loadScreens(); 
-		this.screenNotPassedIndex = this.screensNotPassed.length - 1;
+		this.screenNotPassedIndex = 0;
 		console.log(this.screens);
 	}
     
@@ -360,7 +360,7 @@ class Lesson {
 				this.nScreens++;
                 
 				//Add to the beggining of the list
-				this.screensNotPassed.unshift(this.nScreens);
+				this.screensNotPassed.push(this.nScreens);
             
 				let screenXML = this.lessonXML.childNodes[i];
 				let screen = null;
@@ -404,10 +404,10 @@ class Lesson {
 		//Hide previous screen
 		this.screens[this.currentScreenNumber].hide();
         
-		this.screenNotPassedIndex--;
+		this.screenNotPassedIndex++;
         
-		if (this.screenNotPassedIndex < 0) {
-			this.screenNotPassedIndex = this.screensNotPassed.length - 1;
+		if (this.screenNotPassedIndex == this.nScreens-1) {
+			this.screenNotPassedIndex = 0;
 		}
         
 		this.currentScreenNumber = this.screensNotPassed[this.screenNotPassedIndex];
